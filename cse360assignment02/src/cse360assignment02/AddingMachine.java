c9e23a8dcaaf39feefb33907971d5a78edb16e0c
luuli <luuli@Hermes>
2020-10-05 20:34:32 -0700

    Changed all methods and added a new variable.

--- cse360assignment02/src/cse360assignment02/AddingMachine.java
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -1,44 +1,62 @@
 package cse360assignment02;
+/*
+ * @author Luuli Hoang
+ * @version 2.0
+ */
 
 public class AddingMachine {
 	private int total;
+	private String memory;
 	
 	/*
-	 * constructor
+	 * This is the constructor for AddingMachine that instantiates the variables
 	 */
 	public AddingMachine () {
 		total = 0; // not needed - included for clarity
+		memory = "0";
 	}
 	
 	/*
-	 * gets total (value)
+	 * This method returns the total value of AddingMachine
+	 * @return the AddingMachine's total
 	 */
 	public int getTotal () { 
-		return 0;
+		return total;
 	}
 	
 	/*
-	 * adds value
+	 * This method adds the parameter to the AddingMachine's total and
+	 * updates the memory string to be used by toString
+	 * @param value value to add to the AddingMachine's total and memory
 	 */
 	public void add (int value) { 
+		total += value;
+		memory += " + " + value;
 	}
 	
 	/*
-	 * subtracts value
+	 * This method subtracts the parameter to the AddingMachine's total and
+	 * updates the memory string to be used by toString
+	 * @param value value to subtract from the AddingMachine's total and add to the memory
 	 */
 	public void subtract (int value) { 
+		total -= value;
+		memory += " - " + value;
 	}
 	
 	/*
-	 * turns into String
+	 * This method returns all the operations of the AddingMachine
+	 * @return the AddingMachine's operations as a string
 	 */
 	public String toString () { 
-		return "";
+		return memory;
 	}
 	
 	/*
-	 * clears total
+	 * This method resets the AddingMachine's total and memory string
 	 */
 	public void clear() { 
+		total = 0;
+		memory = "0";
 	}
 }
